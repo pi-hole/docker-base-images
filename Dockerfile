427e3fb78f07038d8ccb897dd40fd17de476df6e
Adam Hill <adam@diginc.us>
2018-10-31 22:32:19 -0500

    remove s6 from base image
    s6 has to be installed in the ARM variant images, so docker-pi-hole is a better location
    
    Signed-off-by: Adam Hill <adam@diginc.us>

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
 FROM debian:stretch-slim
-COPY create_pam_debians.sh /
-RUN bash -ex /create_pam_debians.sh
+COPY create_pam_debians.sh /usr/local/bin/
+RUN bash -ex create_pam_debians.sh
 
 FROM debian:stretch-slim
 COPY --from=0 /tmp/*.deb /
-ENV S6OVERLAY_RELEASE https://github.com/just-containers/s6-overlay/releases/download/v1.21.4.0/s6-overlay-amd64.tar.gz
-COPY setup.sh /usr/local/bin/setup.sh
-RUN bash -ex setup.sh 2>&1
+COPY setup.sh /usr/local/bin/
+RUN bash -ex setup.sh
--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -ex
 # Seperated specifically to enable easy modificaiton of Dockerfile to cache this really long step
 
-# Install s6 and pam requirements
+# Install pam requirements
 apt-get update
-apt-get install -y curl procps libc6-dev libc-dev
-curl -L -s $S6OVERLAY_RELEASE | tar xvzf - -C /
-mv /init /s6-init
+apt-get install -y libc6-dev libc-dev
 
 # From file from earlier docker file
 # https://github.com/pi-hole/docker-pi-hole/issues/243
 dpkg -i libpam-doc*.deb libpam-modules*.deb libpam-runtime*.deb libpam0g*.deb
 rm /*.deb
+rm -rf /var/cache/apt/archives /var/lib/apt/lists/*

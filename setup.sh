10e08bd1473d66cc6ed9eea9d037df752cbb784f
DistractionRectangle <48034372+DistractionRectangle@users.noreply.github.com>
2019-05-19 10:09:05 -0700

    Update setup.sh
    Only patch default packages
    libpam-docs, libpam0g-dev, and various debug symbol packages are
    not apart of debian:stretch-slim by default. Furthermore, they are
    not needed by the downstream pihole-docker images.
    
    Matching against the package name delimiter "_" avoids installing
    debug symbol packages and libpam0g-dev.
    
    lib6c-dev and libc-dev are only needed as libpam0g-dev dependencies
    in this image, so they are also removed.
    
    Signed-off-by: DistractionRectangle <48034372+DistractionRectangle@users.noreply.github.com>

--- setup.sh
+++ setup.sh
@@ -1,12 +1,10 @@
 #!/bin/bash -ex
 # Seperated specifically to enable easy modificaiton of Dockerfile to cache this really long step
 
-# Install pam requirements
-apt-get update
-apt-get install -y libc6-dev libc-dev
-
 # From file from earlier docker file
 # https://github.com/pi-hole/docker-pi-hole/issues/243
-dpkg -i libpam-doc*.deb libpam-modules*.deb libpam-runtime*.deb libpam0g*.deb
+dpkg -i libpam-modules_*.deb \
+        libpam-modules-bin_*.deb \
+        libpam-runtime_*.deb \
+        libpam0g_*.deb
 rm /*.deb
-rm -rf /var/cache/apt/archives /var/lib/apt/lists/*

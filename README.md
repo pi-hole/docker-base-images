03500de9c93e405d3d117c63765b1c2b0b07cade
Daniel <daniel@developerdan.com>
2020-05-26 00:35:13 -0400

    Added tagging logic for debian-base to support both Stretch and Buster build tags.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # pihole/docker-base-images
 
-![Image Builds](https://github.com/pi-hole/docker-base-images/workflows/Image%20Builds/badge.svg)
+[![Image Builds](https://github.com/pi-hole/docker-base-images/workflows/Image%20Builds/badge.svg)](https://github.com/pi-hole/docker-base-images/actions)
 
-Docker images used by other processes within pihole
+Docker images used by other processes within pihole.
 
 ## Debian Base [![Docker Pulls](https://img.shields.io/docker/pulls/pihole/debian-base)](https://hub.docker.com/r/pihole/debian-base)
 
@@ -11,16 +11,21 @@ The pihole/docker-pi-hole aka docker image `pihole/pihole` uses this for x86 arc
 Tag Patterns:
 * The standard `latest` tag, which is always in sync with the `master` branch
 * Version tags, such as `v1.1`. Version tags are for a specific release of Dockerfile, however there are still regular builds to pull in changes from the base Debian layers, such as security updates.
-* Branch based tags. These are always in sync with the branch matching the tag name and should only be used for testing and development purposes
+* Branch based tags. These are always in sync with the branch matching the tag name and should only be used for testing and development purposes.
+* Debian based tags: `buster`, `stretch`, `v1.1-buster`, `v1.1-stretch`, `dev-buster`, `dev-stretch`.
 
 ## FTL Images [![Docker Pulls](https://img.shields.io/docker/pulls/pihole/ftl-build)](https://hub.docker.com/r/pihole/ftl-build)
 
-All of the images used by the pihole/FTL repo to build the contents of that repo
+All the images used by the pihole/FTL repo to build the contents of that repo
 
 Tag Patterns:
 * Architecture tags, such as `x86_64` and `arm`. These are always the latest builds for the given architecture type
 * Version tags with architecture, such as `v1.1-x86_64` and `v1.1-arm`. Version tags are for a specific release of Dockerfiles, however there are still regular builds to pull in changes from the base Debian layers, such as security updates.
-* Branch based tags. These are always in sync with the branch matching the tag name and should only be used for testing and development purposes
+* Branch based tags. These are always in sync with the branch matching the tag name and should only be used for testing and development purposes.
+
+## Web Build Image [![Docker Pulls](https://img.shields.io/docker/pulls/pihole/web-build)](https://hub.docker.com/r/pihole/web-build)
+
+Build image used by the [web repo](https://github.com/pi-hole/web)
 
 ## How does it get uploaded?
 
--- tag-and-push.sh
+++ tag-and-push.sh
@@ -1,42 +1,68 @@
 #!/bin/bash -e
 
+# Usage: ./tag-and-push.sh ${docker-compose-service-name}
+# examples:
+#     ./tag-and-push.sh debian-base-buster
+#     ./tag-and-push.sh ftl-build_aarch64
+#
+# DRY_RUN=true to skip pushing images
+#
 function main() {
   target="${1}"
   current_git_tag=$(git describe --tags --exact-match 2> /dev/null || true)
   current_branch=$(git rev-parse --abbrev-ref HEAD | sed "s/\//-/g")
 
-  # Push debian and web-build tags
-  if [ "${target}" = "debian-base" ] || [ "${target}" = "web-build" ]; then
-      tag_and_push "pihole/${target}" "latest" "${current_git_tag}"
-      branch_based_tag=$(build_branch_tag "${current_branch}")
-      tag_and_push "pihole/${target}" "latest" "${branch_based_tag}"
+  # Push debian tags
+  if [[ "${target:0:11}" = "debian-base" ]]; then
+    flavor=${target:12:$$}
+    # TODO set buster as the primary version for `latest` and tag-based builds
+    primary_debian_version="stretch"
+    branch_based_tag=$(build_branch_tag "${current_branch}")
+    flavored_git_tag_based_tag=$(build_flavored_git_tag_tag "${flavor}" "${current_git_tag}")
+    flavored_branch_name=$(build_flavored_branch_name "${current_branch}" "${flavor}")
+
+    if [[ "${flavor}" = "${primary_debian_version}" ]]; then
+      tag_and_push "pihole/debian-base" "${flavor}" "${current_git_tag}"
+      tag_and_push "pihole/debian-base" "${flavor}" "${flavored_git_tag_based_tag}"
+      tag_and_push "pihole/debian-base" "${flavor}" "${branch_based_tag}"
+      tag_and_push "pihole/debian-base" "${flavor}" "${flavored_branch_name}"
+    else
+      tag_and_push "pihole/debian-base" "${flavor}" "${flavored_branch_name}"
+      tag_and_push "pihole/debian-base" "${flavor}" "${flavored_git_tag_based_tag}"
+    fi
   fi
 
   # Push web-build node version
-  if [ "${target}" = "web-build" ]; then
-      tag_and_push "pihole/${target}" "latest" "12"
+  if [[ "${target}" = "web-build" ]]; then
+    branch_based_tag=$(build_branch_tag "${current_branch}")
+    tag_and_push "pihole/web-build" "latest" "${current_git_tag}"
+    tag_and_push "pihole/web-build" "latest" "${branch_based_tag}"
+    tag_and_push "pihole/web-build" "latest" "12"
   fi
 
   # Push FTL tags
-  if [ "${target:0:9}" = "ftl-build" ]; then
+  if [[ "${target:0:9}" = "ftl-build" ]]; then
     flavor=${target:10:$$}
     branch_based_tag=$(build_ftl_branch_tag "${flavor}" "${current_branch}")
     tag_and_push "pihole/ftl-build" "${flavor}" "${branch_based_tag}"
 
-    git_tag_based_tag=$(build_ftl_git_tag_tag "${flavor}" "${current_git_tag}")
+    git_tag_based_tag=$(build_flavored_git_tag_tag "${flavor}" "${current_git_tag}")
     tag_and_push "pihole/ftl-build" "${flavor}" "${git_tag_based_tag}"
   fi
 }
 
 # Tag and push a docker image if the given tag isn't empty
+# Set environment var `DRY_RUN=true` to disable docker push.
 function tag_and_push() {
   image="${1}"
   local_tag="${2}"
   target_tag="${3}"
-  if [ -n "${target_tag}" ]; then
-    echo "Tagging ${image}:${target_tag}"
+  if [[ -n "${target_tag}" ]]; then
+    echo "Tagging ${image}:${local_tag} as ${target_tag}"
     docker tag "${image}:${local_tag}" "${image}:${target_tag}"
-    docker push "${image}:${target_tag}"
+    if [[ "${DRY_RUN}" != "true" ]]; then
+        docker push "${image}:${target_tag}"
+    fi
   fi
 }
 
@@ -47,10 +73,21 @@ function tag_and_push() {
 function build_branch_tag() {
   branch="${1}"
 
-  if [ "${branch}" = "master" ]; then
+  if [[ "${branch}" = "master" ]]; then
     echo "latest"
   else
     echo "${branch}"
+  fi
+}
+
+function build_flavored_branch_name() {
+  branch="${1}"
+  flavor="${2}"
+
+  if [[ "${branch}" = "master" ]]; then
+    echo "${flavor}"
+  else
+    echo "${branch}-${flavor}"
   fi
 }
 
@@ -62,22 +99,23 @@ function build_ftl_branch_tag() {
   flavor="${1}"
   branch="${2}"
 
-  if [ "${branch}" = "master" ]; then
+  if [[ "${branch}" = "master" ]]; then
     echo "${flavor}"
   else
     echo "${branch}-${flavor}"
   fi
 }
 
-# Build a docker tag based on the FTL architecture flavor and git tag.
+# Build a docker tag based on the FTL-architecture/debian flavor and git tag.
 # examples:
 #     tag v1.1: "v1.1-x86_64-musl"
+#     tag v1.1: "v1.1-buster"
 #     no tag: ""
-function build_ftl_git_tag_tag() {
+function build_flavored_git_tag_tag() {
   flavor="${1}"
   git_tag="${2}"
 
-  if [ -n "${git_tag}" ]; then
+  if [[ -n "${git_tag}" ]]; then
     echo "${git_tag}-${flavor}"
   fi
 }

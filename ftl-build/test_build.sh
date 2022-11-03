7f76814fdb0a0cdb2b09296a747ecca629acea63
DL6ER <dl6er@dl6er.de>
2022-11-03 23:34:37 +0100

    Compile nettle from source instead of relying on whatever version is provided by the building distribution for all architectures but armv5te (armel and amd64 libc6 conflict in debian:buster)
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- ftl-build/test_build.sh
+++ ftl-build/test_build.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+COL_NC="\x1b[0m"
+COL_BOLD="\x1b[1m"
+COL_GREEN="\x1b[32m"
+COL_RED="\x1b[91m"
+
 # Walk subdirectories
 for dir in ./*/
 do
         pushd "$dir"
         if ! docker build .; then
-          echo "$dir - FAILED"
+          echo -e "${COL_BOLD}  $dir - ${COL_RED}FAILED${COL_NC}"
           exit 1
         else
-          echo "$dir - SUCCESS"
+          echo -e "${COL_BOLD}  $dir - ${COL_GREEN}SUCCESS${COL_NC}"
         fi
         popd
 done
